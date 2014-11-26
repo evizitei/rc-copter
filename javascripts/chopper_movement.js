48416df00df7ffc504d6e56d2eeb6f660452e285
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-25 19:18:55 -0700

    improve chopper movement dynamics

--- javascripts/chopper_movement.js
+++ javascripts/chopper_movement.js
@@ -8,8 +8,12 @@ var ChopperMovement = function(sprite){
   }
   this.maxSpeed = 5;
   this.minSpeed = 0;
-  this.speedIncrement = 0.15;
+  this.speedIncrement = 0.1;
   this.speedDecrement = 0.04;
+  this.rotationIncrement = 0.03;
+  this.rotationDecrement = 0.02;
+  this.maxRotation = 0.65;
+  this.rotation = 0;
 };
 
 ChopperMovement.prototype.increaseSpeed = function(direction){
@@ -18,14 +22,31 @@ ChopperMovement.prototype.increaseSpeed = function(direction){
   }else{
     this.speed[direction] += this.speedIncrement;
   }
+
+  if(direction === 'right'){
+    if(this.rotation >= this.maxRotation){
+      this.rotation = this.maxRotation;
+    }else{
+      this.rotation += this.rotationIncrement;
+    }
+  }else if(direction === 'left'){
+    this.rotation -= this.rotationIncrement;
+    if(this.rotation <= (this.maxRotation * -1)){
+      this.rotation = this.maxRotation * -1;
+    }else{
+      this.rotation -= this.rotationIncrement;
+    }
+  }
 };
 
 ChopperMovement.prototype.move = function(){
   this.chopper.position.x += (this.speed.right - this.speed.left);
   this.chopper.position.y += (this.speed.down - this.speed.up);
+  this.chopper.rotation = this.rotation;
 };
 
 ChopperMovement.prototype.reduceSpeed = function(keyMap){
+  //adjust speed
   Object.keys(keyMap).forEach(function(key){
     if((!keyMap[key]) && this.speed[key] > 0){
       this.speed[key] -= this.speedDecrement;
@@ -33,6 +54,24 @@ ChopperMovement.prototype.reduceSpeed = function(keyMap){
       this.speed[key] = 0;
     }
   }, this);
+
+  //adjust rotation
+  if((!keyMap['right']) && this.rotation > 0){
+    if(this.rotation < this.rotationDecrement){
+      this.rotation = 0;
+    }else{
+      this.rotation -= this.rotationDecrement;
+    }
+  }
+
+  if((!keyMap['left']) && this.rotation < 0){
+    if(this.rotation > (this.rotationDecrement * -1)){
+      this.rotation = 0;
+    }else{
+      this.rotation += this.rotationDecrement;
+    }
+  }
+
 };
 
 module.exports = ChopperMovement;
