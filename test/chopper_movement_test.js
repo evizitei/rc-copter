27b083a035afba78b61be9d2fe76c9d680991865
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-27 12:25:38 -0700

    add tests for chopper movement and split up some long methods

--- test/chopper_movement_test.js
+++ test/chopper_movement_test.js
@@ -33,4 +33,54 @@ describe("ChopperMovement", function(){
       expect(sprite.position).to.eql({x:mover.maxX,y:300});
     });
   });
+
+  describe("#reset", function(){
+    it("re-anchors the sprite", function(){
+      sprite.anchor.x = 1;
+      mover.reset();
+      expect(sprite.anchor.x).to.equal(0.5);
+    });
+
+    it("puts the sprite back in starting position", function(){
+      sprite.position = {x:1, y:1};
+      mover.reset();
+      expect(sprite.position).to.eql({x: 400, y: 150});
+    });
+
+    it("kills speed in all directions", function(){
+      mover.speed = {left:2,right:1,up:4,down:3};
+      mover.reset();
+      expect(mover.speed).to.eql({left:0,right:0,up:0,down:0});
+    });
+  });
+
+  describe("#increaseSpeed", function(){
+    it("increases directional speed by the default increment", function(){
+      mover.speed = {left:0,right:0,up:0,down:0};
+      mover.increaseSpeed("right");
+      expect(mover.speed.right).to.eql(mover.speedIncrement);
+    });
+
+    it("increases rotation when moving right", function(){
+      mover.increaseSpeed("right");
+      expect(mover.rotation).to.eql(mover.rotationIncrement);
+    });
+
+    it("decreases rotation when moving left", function(){
+      mover.increaseSpeed("left");
+      expect(mover.rotation).to.eql(mover.rotationIncrement * -1);
+    });
+
+    it("wont increase speed past max", function(){
+      mover.speed.up = mover.maxSpeed;
+      mover.increaseSpeed("up");
+      expect(mover.speed.up).to.equal(mover.maxSpeed);
+    });
+
+    it("wont rotate past max", function(){
+      mover.rotation = mover.maxRotation;
+      mover.increaseSpeed("right");
+      expect(mover.rotation).to.equal(mover.maxRotation);
+    });
+  });
 });
