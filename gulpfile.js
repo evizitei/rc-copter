3c4e752244935252d5be1d136833100aeb202890
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-27 09:11:40 -0700

    move javascripts to lib, introduce jshint, and cleanup some lint issues.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jasmine = require('gulp-jasmine');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish-ex');
 var webpack = require('gulp-webpack');
 var webpackConfig = require('./webpack.config.js');
 
-gulp.task('default', function(){
-});
-
 gulp.task('webpack:build', function(){
   var myConfig = Object.create(webpackConfig);
-  return gulp.src('javascripts/entry.js')
+  return gulp.src('lib/entry.js')
     .pipe(webpack(myConfig))
     .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('lint', function(){
+  var source = ['./lib/*.js', '!./lib/*.min.js'];
+  return gulp.src(source)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter( stylish ));
+});
+
+gulp.task('default', function(){
+  return gulp.src('spec/test.js')
+    .pipe(jasmine());
 });
