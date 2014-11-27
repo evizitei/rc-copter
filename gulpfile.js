3922a569b354253d90df41c02197b9f996f3df61
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-27 12:31:48 -0700

    don't mix quote types in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var mocha = require('gulp-mocha');
-var stylish = require('jshint-stylish-ex');
-var webpack = require('gulp-webpack');
-var webpackConfig = require('./webpack.config.js');
+var gulp = require("gulp");
+var jshint = require("gulp-jshint");
+var mocha = require("gulp-mocha");
+var stylish = require("jshint-stylish-ex");
+var webpack = require("gulp-webpack");
+var webpackConfig = require("./webpack.config.js");
 
-gulp.task('webpack:build', function(){
+gulp.task("webpack:build", function(){
   "use strict";
   var myConfig = Object.create(webpackConfig);
-  return gulp.src('lib/entry.js')
+  return gulp.src("lib/entry.js")
     .pipe(webpack(myConfig))
-    .pipe(gulp.dest('./public/'));
+    .pipe(gulp.dest("./public/"));
 });
 
-gulp.task('lint', function(){
+gulp.task("lint", function(){
   "use strict";
-  var source = ['./lib/*.js', './test/*.js'];
+  var source = ["./lib/*.js", "./test/*.js"];
   return gulp.src(source)
-    .pipe(jshint('.jshintrc'))
+    .pipe(jshint(".jshintrc"))
     .pipe(jshint.reporter( stylish ));
 });
 
 
-gulp.task('test', function () {
+gulp.task("test", function () {
   "use strict";
-  return gulp.src('test/test.js', {read: false})
-    .pipe(mocha({reporter: 'nyan'}));
+  return gulp.src("test/test.js", {read: false})
+    .pipe(mocha({reporter: "nyan"}));
 });
