161454ca7c778100aeb6939a453e494b9e8c9846
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-27 09:33:33 -0700

    remove jasmine node from gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var jasmine = require('gulp-jasmine');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish-ex');
 var webpack = require('gulp-webpack');
@@ -18,9 +17,4 @@ gulp.task('lint', function(){
   return gulp.src(source)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter( stylish ));
-});
-
-gulp.task('default', function(){
-  return gulp.src('spec/test.js')
-    .pipe(jasmine());
 });
